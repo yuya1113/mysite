7383cd0ac50d0884e2527649b49211697f1dd469
yuya <b5204009@yahoo.co.jp>
2022-01-22 12:43:39 +0900

    test

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-window.alert("プログラム実行中");
+var num =0;
+var type1 = typeof num;
+var str = 'こんにちは';
+var type2 = typeof str;
+var bool = true;
+var type3 = typeof bool;
+var func = ()=>{return 0;};
+var type4 = typeof func;
+var ary = [1, 2, 3];
+var type5 = typeof ary;
+var x;
+var type6 = typeof x;
+
+window.alert(type1 + '\n' + type2 + '\n' + type3 + '\n' + type4 + '\n' + type5 + '\n' + type6);
